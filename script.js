0c1997ab16a840d52cdb43c4374712658ffe4531
Eesa Zahed <eszhd1@gmail.com>
2024-08-07 18:40:08 -0400

    added audio

--- script.js
+++ script.js
@@ -10,5 +10,8 @@ const numbers = [1, 2, 3, 4, 5];
 button.addEventListener("click", () => {
   const rndInt = randomIntFromInterval(1, 5);
 
+  var audio = new Audio("./assets/sounds/erm_what_the_sigma.mp3");
+  audio.play();
+
   console.log(rndInt);
 });
