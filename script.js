ac8ec6958dc85e3841d9522e5240d14bafa28b06
Eesa Zahed <eszhd1@gmail.com>
2024-08-07 20:42:10 -0400

    changed button content

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 const button = document.querySelector("button");
+let currentAudio = null;
 
-// https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
-
-const randomIntFromInterval = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1) + min);
-
-const numbers = [1, 2, 3, 4, 5];
+const randomInt = () => Math.floor(Math.random() * 10 + 1);
 
 button.addEventListener("click", () => {
-  const rndInt = randomIntFromInterval(1, 5);
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+  }
 
-  var audio = new Audio("./assets/sounds/erm_what_the_sigma.mp3");
-  audio.play();
-
-  console.log(rndInt);
+  currentAudio = new Audio(`./assets/sounds/${randomInt()}.mp3`);
+  currentAudio.play();
 });
