a8fc5139598dfab9fa3336e622d3a3dd3ce8b5e6
Eesa Zahed <eszhd1@gmail.com>
2024-08-08 17:00:33 -0400

    add sound description

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# soundboard
+# soundboard ([View Live](https://eesazahed.github.io/soundboard))
+
+Click to play a random meme/trending audio
--- script.js
+++ script.js
@@ -1,14 +1,39 @@
-const button = document.querySelector("button");
+const button = document.querySelector("#play");
+const nowPlaying = document.querySelector("#now-playing");
+
 let currentAudio = null;
 
-const randomInt = () => Math.floor(Math.random() * 10 + 1);
+const audios = {
+  1: "mewing",
+  2: "english or spanish",
+  3: "erm what the sigma",
+  4: "lets go gambling!",
+  5: "nya ichi ni san",
+  6: "oi oi... baka",
+  7: "sigma trillionare grindset",
+  8: "skibidi fortnite",
+  9: "skibidi dop dop dop",
+  10: "rizz",
+  11: "FE!N",
+  12: "waitwaitwait",
+};
+
+const randomInt = () =>
+  Math.floor(Math.random() * Object.keys(audios).length + 1);
 
 button.addEventListener("click", () => {
+  if (nowPlaying.style.opacity !== 1) {
+    nowPlaying.style.opacity = 1;
+  }
+
   if (currentAudio) {
     currentAudio.pause();
     currentAudio.currentTime = 0;
   }
 
-  currentAudio = new Audio(`./assets/sounds/${randomInt()}.mp3`);
+  const randomId = randomInt();
+
+  nowPlaying.innerText = audios[randomId];
+  currentAudio = new Audio(`./assets/sounds/${randomId}.mp3`);
   currentAudio.play();
 });
